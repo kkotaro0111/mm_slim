42f4f406583a93d11bbefd628d3a2560dfb14681
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2016-02-16 18:31:30 +0900

    Modify: convert to pretty format with slim

--- config.rb
+++ config.rb
@@ -12,6 +12,10 @@ page '/*.txt', layout: false
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
+# Set slim-lang output style
+Slim::Engine.set_default_options :pretty => true
+
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
